f6a8ed0bb8e1b1223a2f75a36236d6394c4d06b7
student <gitzpitadek@gmail.com>
2022-03-14 18:34:58 +0100

    CHANGES IN MAIN.JAVA

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("linia z gałęzią secondBranch");
+        System.out.println("DRUGA LINIA TEGO CZEGIŚ");
 	// write your code here
     }
 }

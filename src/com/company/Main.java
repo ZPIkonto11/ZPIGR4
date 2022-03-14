4adb8889232395224a198049324a32c0608564a8
ZPIkonto11 <101585589+ZPIkonto11@users.noreply.github.com>
2022-03-14 18:38:35 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+	    System.out.println("1.1");
+        System.out.println("DRUGA LINIA TEGO CZEGIŚ");
 	// write your code here
     }
 }
